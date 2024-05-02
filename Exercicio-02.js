86aa01767bad5feab3773fa9f094a49520cca3ee
Danielchampion5 <danielchampionoficial@gmail.com>
2024-05-02 17:08:02 -0300

    feat: exercicio 02

---
+++ Exercicio-02.js
@@ -0,0 +1,19 @@
+let dia = parseInt( prompt("Insira um número de 1 a 7."));
+
+switch(dia) {
+    case 1: alert("Segunda-feira"); break;
+
+    case 2: alert("Terca-feira"); break;
+
+    case 3: alert("Quarta-feira"); break;
+
+    case 4: alert("Quinta-feira"); break;
+
+    case 5: alert("Sexta-feira"); break;
+
+    case 6: alert("Sabado"); break;
+
+    case 7: alert("Domingo"); break;
+
+    default: alert("Numero invalido inserido.");
+}
