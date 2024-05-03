c3f9881b7e20ad2b33d35ed87a27ee34d94accde
Danielchampion5 <danielchampionoficial@gmail.com>
2024-05-03 14:30:17 -0300

    feat: exercicio-06

---
+++ exercicio-06.js
@@ -0,0 +1,13 @@
+let ano = parseInt(prompt("Informe seu ano de nascimento."))
+
+let idade = 2024-ano;
+
+console.log(idade)
+
+if(idade >= 18) {
+    alert("Você é maior de idade.")
+}
+
+else{
+  alert("Você não é maior de idade.")
+}
